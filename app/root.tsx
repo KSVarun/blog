5d83b9a3fd0f5676636d3e1805c7995ea830d852
KSVarun <ksvarun001@gmail.com>
2022-03-05 15:50:20 +0530

    added analytics script

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,12 @@ import {
 } from 'remix';
 import styles from './tailwind.css';
 
+declare global {
+  interface Window {
+    dataLayer: any;
+  }
+}
+
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }];
 };
@@ -26,6 +32,15 @@ export default function App() {
         <meta name='viewport' content='width=device-width,initial-scale=1' />
         <Meta />
         <Links />
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-9PRELERZXT'
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {window.dataLayer.push(arguments)}; gtag('js', new Date());
+          gtag('config', 'G-9PRELERZXT');
+        </script>
       </head>
       <body>
         <Outlet />
