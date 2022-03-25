d267bd38d8acfb512cf423bd9ef7e08ee1f2e003
KSVarun <ksvarun001@gmail.com>
2022-03-26 01:41:14 +0530

    add md5sum in js and date to posts section

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunction, useLoaderData, Link, LinksFunction } from 'remix';
 import defaultStyles from '../../default.css';
 import * as placeholdersForImages from './placeholders-for-images.mdx';
+import * as md5sumInJs from './md5sum-in-js.mdx';
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: defaultStyles }];
@@ -10,15 +11,17 @@ function postsFromModule(mod: any) {
   return {
     slug: mod.filename.replace(/\.mdx?$/, ''),
     ...mod.attributes.meta,
+    date: mod.attributes.date,
   };
 }
 
 export const loader: LoaderFunction = () => {
-  return [postsFromModule(placeholdersForImages)];
+  return [postsFromModule(placeholdersForImages), postsFromModule(md5sumInJs)];
 };
 
 export default function PostsIndex() {
   const posts = useLoaderData();
+  console.log(posts);
   return (
     <div>
       <div>
@@ -27,8 +30,11 @@ export default function PostsIndex() {
             <Link to={post.slug} className='text-xl link'>
               {post.title}
             </Link>
+            {post.date ? (
+              <p className='mt-0 mb-1 text-xs'>{post.date}</p>
+            ) : null}
             {post.description ? (
-              <p className='m-0 lg:m-0 mt-2'>{post.description}</p>
+              <p className='mt-0 mb-6'>{post.description}</p>
             ) : null}
           </div>
         ))}
